8df14bb72119fee76d9ac6f7bfc35a3c9e59d7e4
Sven Nesfeder <154921029+haimseven@users.noreply.github.com>
2024-01-10 14:45:58 -0500

    Additions to script.js
    Additions include:
    - Function getComputerChoice
    - Function playRound
    - Comments of functionaliy for created functions

---
+++ script.js
@@ -0,0 +1,13 @@
+function getComputerChoice(){
+
+}
+// Randomly returns either Rock, Paper, or Scissors
+
+
+function playRound(playerSelection,computerSelection) {
+
+}
+// Plays a single round of Rock Paper Scissors using playerSelection and computerSelection.
+// Returns a string that declares the winner of round
+// If round ends in a Tie, the round restarts
+
