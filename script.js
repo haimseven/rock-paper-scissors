a9e8a2e2288c6f7e893ffa4408ae12fbcb224fff
Sven Nesfeder <154921029+haimseven@users.noreply.github.com>
2024-01-10 15:12:48 -0500

    Edit to getComputerChoice
    Edit includes:
    - Added constant randomNumber
    - Added switch statment

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-function getComputerChoice(){
-
-}
 // Randomly returns either Rock, Paper, or Scissors
+function getComputerChoice(){
+    // Generate a random number between 0 and 2
+    const randomNumber = Math.floor(Math.random() * 3);
+
+    // Assigns Rock, Paper, or Scissors based on the random number 
+    switch(randomNumber){
+        case 0:
+            return 'Rock';
+        case 1:
+            return 'Paper';
+        case 2:
+            return 'Scissors';
+        default:
+            return 'Rock';
+    }
+}
 
 
 function playRound(playerSelection,computerSelection) {
